8b82b1e21a31fc099e16cbbc44613b09706797a9
dao van tung <tungdao123pzo@gmail.com>
2024-09-01 14:38:25 +0700

    up

--- javascipt.js
+++ javascipt.js
@@ -17,3 +17,47 @@ console.log(c);
 
 
 console.log("heiieeh");
+
+a = 5;
+b =10;
+console.log(a + b);
+
+console.log(a - b);
+
+console.log(a * b);
+
+console.log(a ** b);
+
+console.log(a % b);
+console.log(a / b);
+console.log("4565654fdsf" + "dao van tung");
+
+console.log(7 + 5);
+
+console.log(a++);
+
+console.log(a++);
+console.log(6 + 7 + "tungao");
+
+console.log(--a);
+console.log(a--);
+console.log(a);
+
+a = 20;
+var d = a++ - --a + ++a;
+console.log(d);
+
+a =20;
+var d = a++ - a-- + --a * 3;
+console.log(d);
+
+console.log(a);
+
+a += 10;
+console.log(a);
+
+console.log(Math.floor(5 / 10));
+var b = 5;
+b %= 2;
+
+console.log(b);
