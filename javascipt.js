2d8d489b0fd829a37502c2de876f4d4e8b7d5f7d
dao van tung <tungdao123pzo@gmail.com>
2024-09-01 13:48:07 +0700

    update

---
+++ javascipt.js
@@ -0,0 +1,19 @@
+
+
+
+var a = 1;
+var b = 122300;
+var c = a + b;
+
+  var button = document.querySelector("button");
+        button.onclick = function () {
+            alert(c);
+        }
+
+
+
+console.log(c);
+
+
+
+console.log("heiieeh");
